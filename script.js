da487ffd13930b244d794266daff9b05b152f9fe
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2026-01-29 21:42:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-fetch('./employees.json')
-  .then(response => response.json())
-  .then(employees => {
-    const sortedEmployees = employees.sort((a, b) => a.age - b.age);
-    console.log(sortedEmployees);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+window.onload = function () {
+  fetch('./employees.json')
+    .then(response => response.json())
+    .then(employees => {
+      const sortedEmployees = employees.sort((a, b) => a.age - b.age);
+      console.log(sortedEmployees);
+    })
+    .catch(error => console.error(error));
+};
